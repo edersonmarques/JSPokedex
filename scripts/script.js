600236c525093a8df17b56f368e48ab2b3e70505
Éderson Marques <edersondesouzamarques@gmail.com>
2023-05-12 18:45:56 -0300

    adicionado dark mode

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const theme_button = document.querySelector('#theme-button');
+const theme_image = document.querySelector('.button-image');
+let theme = 'light';
+
+theme_button.addEventListener('click', () => {
+    if (theme === 'light') {
+        theme = 'dark';
+        theme_image.src = './images/sun.png';
+    } else if (theme === 'dark') {
+        theme = 'light';
+        theme_image.src = './images/moon.png';
+    } else {
+        console.error('Algo está errado');
+    }
+    body.className = theme;
+});
+
