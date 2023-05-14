db39e45125068f0b74bbc20e0553ca9dc0c62d78
Éderson Marques <edersondesouzamarques@gmail.com>
2023-05-14 02:39:49 -0300

    adicionados 151, porém não estão em ordem

--- scripts/poke-api-requisition.js
+++ scripts/poke-api-requisition.js
@@ -1,29 +1,33 @@
 const pokemon_list = document.querySelector('#pokemon-list');
+let pokemon_number = 1;
 
-fetch('https://pokeapi.co/api/v2/').then(response => {
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response => {
     return response.json();
 }).then(response_body => {
-    createPokemonCard(response_body);
+    console.log(response_body);
+    for (const n of response_body.results) {
+        fetch(n.url).then(response => {
+            return response.json();
+        }).then(response_body => {
+            let new_poke_card = document.createElement('li');
+            pokemon_list.appendChild(new_poke_card);
+            pokemon_list.lastChild.classList.add("pokemon-card");
+            pokemon_list.lastChild.innerHTML = `
+            <div class="informations">
+                <span class="pokemon-name">${response_body.name}</span>
+                <span class="pokemon-number">#${pokemon_number}</span>
+            </div>
+            
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon_number}.gif" alt="Pokemon Gif" class="gif">
+            
+            <ul class="types">
+                <li class="type grass-type">lorem</li>
+                <li class="type poison-type">lorem</li>
+            </ul>
+            
+            <p class="description">lorem ipsum dlor it amet sarut jsnjfbdhfbjf jfkfjkg kdfbbghfg hebfb ghkb frfr grgbkgungj jrgkgjng jrgrng gnfgj g gjgngj gjgngng</p>`;
+            pokemon_number++;
+        });
+    }
 });
 
-
-
-function createPokemonCard(poke_api_return) {
-    let new_poke_card = document.createElement('li');
-    pokemon_list.appendChild(new_poke_card);
-    pokemon_list.lastChild.classList.add("pokemon-card");
-    pokemon_list.lastChild.innerHTML = `
-<div class="informations">
-    <span>Bulbasaur</span>
-    <span>#001</span>
-</div>
-
-<img src="images/bulbasaur.gif" alt="Bulbasaur" class="gif">
-
-<ul class="types">
-    <li class="grass-type type" >Grama</li>
-    <li class="poison-type type" >Veneno</li>
-</ul>
-
-<p class="description">Há uma semente de planta em suas costas desde o dia em que este Pokémon nasceu. A semente cresce lentamente.</p>`;
-}
