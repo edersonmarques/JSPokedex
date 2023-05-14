fb6bd9db1b29dc17d74e29b7f0437265cc34362e
Éderson Marques <edersondesouzamarques@gmail.com>
2023-05-14 09:24:33 -0300

    adicionado favicon

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 const body = document.body;
-const theme_button = document.querySelector('#theme-button');
-const theme_image = document.querySelector('.button-image');
-let theme = 'light';
+const theme_button = document.querySelector("#theme-button");
+const theme_image = document.querySelector(".button-image");
+let theme = "light";
 
-theme_button.addEventListener('click', () => {
-    if (theme === 'light') {
-        theme = 'dark';
-        theme_image.src = './images/sun.png';
-    } else if (theme === 'dark') {
-        theme = 'light';
-        theme_image.src = './images/moon.png';
-    } else {
-        console.error('Algo está errado');
-    }
-    body.className = theme;
+theme_button.addEventListener("click", () => {
+  if (theme === "light") {
+    theme = "dark";
+    theme_image.src = "./images/sun.png";
+  } else if (theme === "dark") {
+    theme = "light";
+    theme_image.src = "./images/moon.png";
+  } else {
+    console.error("Algo está errado");
+  }
+  body.className = theme;
 });
